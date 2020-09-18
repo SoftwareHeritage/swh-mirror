9d3a0dffed4c367f3111dd352d359c826e0699d7
David Douard <david.douard@sdfa3.org>
2020-09-18 14:38:05 +0200

    Do not publish built images to the docker hub by default
    To publish built images, you now need to specify it using:
    
      PUBLISH=yes ./build_images.sh
    
    from the images/ directory.

--- images/build_images.sh
+++ images/build_images.sh
@@ -7,9 +7,12 @@ username=$(docker info | grep Username | awk '{print $2}')
 for img in base web; do
   docker build --build-arg SWH_VER=${builddate} --build-arg debianversion=buster -t softwareheritage/${img}:${builddate} -f ${img}/Dockerfile .
   docker tag softwareheritage/${img}:${builddate} softwareheritage/${img}:latest
-  if [[ X${username} = Xsoftwareheritage ]]; then
-    docker push softwareheritage/${img}:${builddate}
-    docker push softwareheritage/${img}:latest
+  if [[ "${username}" = "softwareheritage" ]] && [[ "${PUBLISH:=no}" = "yes" ]]; then
+	  echo "Publishing image softwareheritage/${img}:${builddate} on docker hub"
+      docker push softwareheritage/${img}:${builddate}
+      docker push softwareheritage/${img}:latest
   fi
 done
 
+echo "Done creating images. You may want to use"
+echo "export SWH_IMAGE_TAG=${builddate}"
